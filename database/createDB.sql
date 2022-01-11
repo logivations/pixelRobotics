c1857d6c69b27443229c5af877f6692d05ca9302
volodymyrboichuk <volodymyr.boichuk@logivations.com>
2022-01-11 11:44:30 +0200

    * change column position

--- database/createDB.sql
+++ database/createDB.sql
@@ -7,13 +7,13 @@ create table if not exists user_activity
     ID                  varchar(64)    not null,
     user_IP             varchar(48)    not null,
     visited_page        varchar(48)    not null,
+    user_agent          varchar(255)   not null,
+    provider_detail     varchar(64)    not null,
     date_time           datetime       not null default CURRENT_TIMESTAMP,
     cookie_detail       mediumtext     not null,
-    user_agent          varchar(255)   not null,
     number_of_visits    int            not null default 0,
     plugins             varchar(64)    not null default '',
     resolution          varchar(32)    not null default '',
-    provider_detail     varchar(64)    not null,
     referer             varchar(32)    not null default '',
     primary key (user_IP, visited_page, user_agent, provider_detail)
 ) engine=InnoDB default charset=latin1;
